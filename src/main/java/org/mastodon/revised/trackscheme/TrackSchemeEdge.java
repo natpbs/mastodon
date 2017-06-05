99c541c0ecd7230db3508113dd5bc0bfe6c3e249
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2017-06-05 22:10:28 +0200

    Let TrackSchemeEdge implements Geometry R-Tree requirements.
    - Distance to a rectangle.
    - Intersects with a rectangle.
    - Mnimal bounding rectangle.

--- src/main/java/org/mastodon/revised/trackscheme/TrackSchemeEdge.java
+++ src/main/java/org/mastodon/revised/trackscheme/TrackSchemeEdge.java
@@ -4,10 +4,18 @@ import org.mastodon.graph.GraphIdBimap;
 import org.mastodon.graph.ref.AbstractEdge;
 import org.mastodon.pool.ByteMappedElement;
 import org.mastodon.pool.PoolObject;
+import org.mastodon.revised.Util;
+import org.mastodon.revised.trackscheme.TrackSchemeEdge.TrackSchemeEdgeGeometry;
 import org.mastodon.revised.trackscheme.TrackSchemeGraph.TrackSchemeEdgePool;
 
-public class TrackSchemeEdge extends AbstractEdge< TrackSchemeEdge, TrackSchemeVertex, TrackSchemeEdgePool, ByteMappedElement >
+import com.github.davidmoten.rtree.Entry;
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Geometry;
+import com.github.davidmoten.rtree.geometry.Rectangle;
+
+public class TrackSchemeEdge extends AbstractEdge< TrackSchemeEdge, TrackSchemeVertex, TrackSchemeEdgePool, ByteMappedElement > implements Entry< TrackSchemeEdge, TrackSchemeEdgeGeometry >
 {
+
 	final ModelGraphWrapper< ?, ? >.ModelEdgeWrapper modelEdge;
 
 	@Override
@@ -60,5 +68,121 @@ public class TrackSchemeEdge extends AbstractEdge< TrackSchemeEdge, TrackSchemeV
 	public void setScreenEdgeIndex( final int screenEdgeIndex )
 	{
 		pool.screenEdgeIndex.setQuiet( this, screenEdgeIndex );
+	}
+
+	/*
+	 * GEOMETRY INTERFACE FOR R-TREES
+	 */
+
+	private final TrackSchemeEdgeGeometry goemetry = new TrackSchemeEdgeGeometry();
+
+	@Override
+	public TrackSchemeEdge value()
+	{
+		return this;
+	}
+
+	@Override
+	public TrackSchemeEdgeGeometry geometry()
+	{
+		return goemetry;
+	}
+
+	public final class TrackSchemeEdgeGeometry implements Geometry
+	{
+
+		private final TrackSchemeVertex ref = vertexPool.createRef();
+
+		private double xt()
+		{
+			return getTarget( ref ).getLayoutX();
+		}
+
+		private double yt()
+		{
+			return getTarget( ref ).getTimepoint();
+		}
+
+		private double xs()
+		{
+			return getSource( ref ).getLayoutX();
+		}
+
+		private double ys()
+		{
+			return getSource( ref ).getTimepoint();
+		}
+
+		@Override
+		public double distance( final Rectangle r )
+		{
+			double x1 = xs();
+			double y1 = ys();
+			double x2 = xt();
+			double y2 = yt();
+			if ( r.contains( x1, y1 ) || r.contains( x2, y2 ) )
+			{
+				return 0.;
+			}
+			else
+			{
+				double d1 = distance( r.x1(), r.y1(), r.x1(), r.y2() );
+				if ( d1 == 0. )
+					return 0;
+				double d2 = distance( r.x1(), r.y2(), r.x2(), r.y2() );
+				if ( d2 == 0. )
+					return 0.;
+				double d3 = distance( r.x2(), r.y2(), r.x2(), r.y1() );
+				double d4 = distance( r.x2(), r.y1(), r.x1(), r.y1() );
+				return Math.min( d1, Math.min( d2, Math.min( d3, d4 ) ) );
+			}
+		}
+
+		private double distance( final float x3, final float y3, final float x4, final float y4 )
+		{
+			double x1 = xs();
+			double y1 = ys();
+			double x2 = xt();
+			double y2 = yt();
+			double d1 = Util.segmentDist( xs(), ys(), x3, y3, x4, y4 );
+			double d2 = Util.segmentDist( xt(), yt(), x3, y3, x4, y4 );
+			double d3 = Util.segmentDist( x3, y3, x1, y1, x2, y2 );
+			if ( d3 == 0. )
+				return 0.;
+			double d4 = Util.segmentDist( x4, y4, x1, y1, x2, y2 );
+			if ( d4 == 0. )
+				return 0.;
+			else
+				return Math.min( d1, Math.min( d2, Math.min( d3, d4 ) ) );
+		}
+
+		@Override
+		public boolean intersects( final Rectangle r )
+		{
+			if ( !mbr().intersects( r ) )
+				return false;
+
+			double x1 = xs();
+			double x2 = xt();
+			double y1 = ys();
+			double y2 = yt();
+
+			return Util.lineSegmentIntersect( x1, y1, x2, y2, r.x1(), r.y1(), r.x1(), r.y2() )
+					|| Util.lineSegmentIntersect( x1, y1, x2, y2, r.x1(), r.y1(), r.x2(), r.y1() )
+					|| Util.lineSegmentIntersect( x1, y1, x2, y2, r.x2(), r.y1(), r.x2(), r.y2() )
+					|| Util.lineSegmentIntersect( x1, y1, x2, y2, r.x1(), r.y2(), r.x2(), r.y2() );
+		}
+
+		@Override
+		public Rectangle mbr()
+		{
+			double x1 = xs();
+			double x2 = xt();
+			double y1 = ys();
+			double y2 = yt();
+			return Geometries.rectangle( Math.min( x1, x2 ), Math.min( y1, y2 ), Math.max( x1, x2 ),
+					Math.max( y1, y2 ) );
+		}
+
 	}
 }
