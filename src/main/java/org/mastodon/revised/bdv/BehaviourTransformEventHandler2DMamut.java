51b1330bd43af28daa4a979f1ed63b5f62f3ab1c
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2019-02-19 22:08:26 +0100

    Finish the BehaviourTransformEventHandler2DMamut.

--- src/main/java/org/mastodon/revised/bdv/BehaviourTransformEventHandler2DMamut.java
+++ src/main/java/org/mastodon/revised/bdv/BehaviourTransformEventHandler2DMamut.java
@@ -1,5 +1,9 @@
 package org.mastodon.revised.bdv;
 
+import org.mastodon.revised.mamut.KeyConfigContexts;
+import org.mastodon.revised.ui.keymap.CommandDescriptionProvider;
+import org.mastodon.revised.ui.keymap.CommandDescriptions;
+import org.scijava.plugin.Plugin;
 import org.scijava.ui.behaviour.ClickBehaviour;
 import org.scijava.ui.behaviour.DragBehaviour;
 import org.scijava.ui.behaviour.ScrollBehaviour;
@@ -16,19 +20,40 @@ public class BehaviourTransformEventHandler2DMamut implements BehaviourTransform
 {
 
 	private static final String DRAG_TRANSLATE = "2d drag translate";
-	private static final String ZOOM_NORMAL = "2d scroll zoom";
 	private static final String DRAG_ROTATE = "2d drag rotate";
-	private static final String SCROLL_ROTATE = "2d scroll rotate";
+	private static final String ZOOM_NORMAL = "2d scroll zoom";
+	private static final String ZOOM_FAST = "2d scroll zoom fast";
+	private static final String ZOOM_SLOW = "2d scroll zoom slow";
 	private static final String SCROLL_TRANSLATE = "2d scroll translate";
+	private static final String SCROLL_TRANSLATE_FAST = "2d scroll translate fast";
+	private static final String SCROLL_TRANSLATE_SLOW = "2d scroll translate slow";
+	private static final String SCROLL_ROTATE = "2d scroll rotate";
+	private static final String SCROLL_ROTATE_FAST = "2d scroll rotate fast";
+	private static final String SCROLL_ROTATE_SLOW = "2d scroll rotate slow";
 	private static final String ROTATE_LEFT = "2d rotate left";
+	private static final String ROTATE_LEFT_FAST = "2d rotate left fast";
+	private static final String ROTATE_LEFT_SLOW = "2d rotate left slow";
 	private static final String ROTATE_RIGHT = "2d rotate right";
+	private static final String ROTATE_RIGHT_FAST = "2d rotate right fast";
+	private static final String ROTATE_RIGHT_SLOW = "2d rotate right slow";
 	private static final String KEY_ZOOM_IN = "2d zoom in";
+	private static final String KEY_ZOOM_IN_FAST = "2d zoom in fast";
+	private static final String KEY_ZOOM_IN_SLOW = "2d zoom in slow";
 	private static final String KEY_ZOOM_OUT = "2d zoom out";
+	private static final String KEY_ZOOM_OUT_FAST = "2d zoom out fast";
+	private static final String KEY_ZOOM_OUT_SLOW = "2d zoom out slow";
 	
 	private static final String[] DRAG_TRANSLATE_KEYS = new String[] { "button2", "button3" };
-	private static final String[] ZOOM_NORMAL_KEYS = new String[] { "meta scroll", "ctrl shift scroll" };
+	private static final String[] ZOOM_NORMAL_KEYS = new String[] { "scroll" };
+	private static final String[] ZOOM_FAST_KEYS = new String[] { "shift scroll" };
+	private static final String[] ZOOM_SLOW_KEYS = new String[] { "ctrl scroll" };
 	private static final String[] DRAG_ROTATE_KEYS = new String[] { "button1" };
-	private static final String[] SCROLL_TRANSLATE_KEYS = new String[] {"not mapped"};
+	private static final String[] SCROLL_TRANSLATE_KEYS = new String[] { "not mapped" };
+	private static final String[] SCROLL_TRANSLATE_FAST_KEYS = new String[] { "not mapped" };
+	private static final String[] SCROLL_TRANSLATE_SLOW_KEYS = new String[] { "not mapped" };
+	private static final String[] SCROLL_ROTATE_KEYS = new String[] { "not mapped" };
+	private static final String[] SCROLL_ROTATE_FAST_KEYS = new String[] { "not mapped" };
+	private static final String[] SCROLL_ROTATE_SLOW_KEYS = new String[] { "not mapped" };
 	private static final String[] ROTATE_LEFT_KEYS = new String[] { "LEFT" };
 	private static final String[] ROTATE_RIGHT_KEYS = new String[] { "RIGHT" };
 	private static final String[] ROTATE_LEFT_FAST_KEYS = new String[] { "shift LEFT" };
@@ -44,46 +69,75 @@ public class BehaviourTransformEventHandler2DMamut implements BehaviourTransform
 	private static final String[] KEY_ZOOM_OUT_SLOW_KEYS = new String[] { "ctrl DOWN" };
 
 	private static final double[] SPEED = { 1.0, 10.0, 0.1 };
-	private static final String[] SPEED_NAME = { "", " fast", " slow" };
-	private static final String[] SPEED_MOD = { "", "shift ", "ctrl " };
 
+	/*
+	 * Command descriptions for all provided commands
+	 */
+	@Plugin( type = Descriptions.class )
+	public static class Descriptions extends CommandDescriptionProvider
+	{
+		public Descriptions()
+		{
+			super( KeyConfigContexts.BIGDATAVIEWER );
+		}
+
+		@Override
+		public void getCommandDescriptions( final CommandDescriptions descriptions )
+		{
+			descriptions.add( DRAG_TRANSLATE, DRAG_TRANSLATE_KEYS, "Pan the view by mouse-dragging." );
+			descriptions.add( DRAG_ROTATE, DRAG_ROTATE_KEYS, "Rotate the view by mouse-dragging." );
+
+			descriptions.add( ZOOM_NORMAL, ZOOM_NORMAL_KEYS, "Zoom in by scrolling." );
+			descriptions.add( ZOOM_FAST, ZOOM_FAST_KEYS, "Zoom in by scrolling (fast)." );
+			descriptions.add( ZOOM_SLOW, ZOOM_SLOW_KEYS, "Zoom in by scrolling (slow)." );
+
+			descriptions.add( SCROLL_TRANSLATE, SCROLL_TRANSLATE_KEYS, "Translate by scrolling." );
+			descriptions.add( SCROLL_TRANSLATE_FAST, SCROLL_TRANSLATE_FAST_KEYS, "Translate by scrolling (fast)." );
+			descriptions.add( SCROLL_TRANSLATE_SLOW, SCROLL_TRANSLATE_SLOW_KEYS, "Translate by scrolling (slow)." );
+
+			descriptions.add( ROTATE_LEFT, ROTATE_LEFT_KEYS, "Rotate left (counter-clockwise) by 1 degree." );
+			descriptions.add( ROTATE_RIGHT, ROTATE_RIGHT_KEYS, "Rotate right (clockwise) by 1 degree." );
+			descriptions.add( KEY_ZOOM_IN, KEY_ZOOM_IN_KEYS, "Zoom in." );
+			descriptions.add( KEY_ZOOM_OUT, KEY_ZOOM_OUT_KEYS, "Zoom out." );
+
+			descriptions.add( ROTATE_LEFT_FAST, ROTATE_LEFT_FAST_KEYS, "Rotate left (counter-clockwise) by 10 degrees." );
+			descriptions.add( ROTATE_RIGHT_FAST, ROTATE_RIGHT_FAST_KEYS, "Rotate right (clockwise) by 10 degrees." );
+			descriptions.add( KEY_ZOOM_IN_FAST, KEY_ZOOM_IN_FAST_KEYS, "Zoom in (fast)." );
+			descriptions.add( KEY_ZOOM_OUT_FAST, KEY_ZOOM_OUT_FAST_KEYS, "Zoom out (fast)." );
+
+			descriptions.add( ROTATE_LEFT_SLOW, ROTATE_LEFT_SLOW_KEYS, "Rotate left (counter-clockwise) by 0.1 degree." );
+			descriptions.add( ROTATE_RIGHT_SLOW, ROTATE_RIGHT_SLOW_KEYS, "Rotate right (clockwise) by 0.1 degree." );
+			descriptions.add( KEY_ZOOM_IN_SLOW, KEY_ZOOM_IN_SLOW_KEYS, "Zoom in (slow)." );
+			descriptions.add( KEY_ZOOM_OUT_SLOW, KEY_ZOOM_OUT_SLOW_KEYS, "Zoom out (slow)." );
+
+			descriptions.add( SCROLL_ROTATE, SCROLL_ROTATE_KEYS, "Rotate by scrolling." );
+			descriptions.add( SCROLL_ROTATE_FAST, SCROLL_ROTATE_FAST_KEYS, "Rotate by scrolling (fast)." );
+			descriptions.add( SCROLL_ROTATE_SLOW, SCROLL_ROTATE_SLOW_KEYS, "Rotate by scrolling (slow)." );
+		}
+	}
 
 	private final DragTranslate dragTranslate;
-
-	private final Zoom zoom;
-
-	private final ScrollTranslate scrollTranslate;
-
 	private final DragRotate dragRotate;
-
+	private final Zoom zoom;
+	private final Zoom zoomFast;
+	private final Zoom zoomSlow;
+	private final ScrollTranslate scrollTranslateNormal;
+	private final ScrollTranslate scrollTranslateFast;
+	private final ScrollTranslate scrollTranslateSlow;
 	private final ScrollRotate scrollRotateNormal;
-
 	private final ScrollRotate scrollRotateFast;
-
 	private final ScrollRotate scrollRotateSlow;
-
 	private final KeyRotate keyRotateLeftNormal;
-
 	private final KeyRotate keyRotateLeftFast;
-
 	private final KeyRotate keyRotateLeftSlow;
-
 	private final KeyRotate keyRotateRightNormal;
-
 	private final KeyRotate keyRotateRightFast;
-
 	private final KeyRotate keyRotateRightSlow;
-
 	private final KeyZoom keyZoomInNormal;
-
 	private final KeyZoom keyZoomInFast;
-
 	private final KeyZoom keyZoomInSlow;
-
 	private final KeyZoom keyZoomOutNormal;
-
 	private final KeyZoom keyZoomOutFast;
-
 	private final KeyZoom keyZoomOutSlow;
 
 	/**
@@ -129,9 +183,15 @@ public class BehaviourTransformEventHandler2DMamut implements BehaviourTransform
 		this.listener = listener;
 
 		dragTranslate = new DragTranslate();
-		zoom = new Zoom();
-		scrollTranslate = new ScrollTranslate();
 		dragRotate = new DragRotate();
+
+		scrollTranslateNormal = new ScrollTranslate( SPEED[ 0 ] );
+		scrollTranslateFast = new ScrollTranslate( SPEED[ 1 ] );
+		scrollTranslateSlow = new ScrollTranslate( SPEED[ 2 ] );
+
+		zoom = new Zoom( SPEED[ 0 ] );
+		zoomFast = new Zoom( SPEED[ 1 ] );
+		zoomSlow = new Zoom( SPEED[ 2 ] );
 
 		scrollRotateNormal = new ScrollRotate( 2 * SPEED[ 0 ] );
 		scrollRotateFast = new ScrollRotate( 2 * SPEED[ 1 ] );
@@ -147,45 +207,42 @@ public class BehaviourTransformEventHandler2DMamut implements BehaviourTransform
 		keyZoomInNormal = new KeyZoom( SPEED[ 0 ] );
 		keyZoomInFast = new KeyZoom( SPEED[ 1 ] );
 		keyZoomInSlow = new KeyZoom( SPEED[ 2 ] );
-		keyZoomOutNormal = new KeyZoom( SPEED[ 0 ] );
-		keyZoomOutFast = new KeyZoom( SPEED[ 1 ] );
-		keyZoomOutSlow = new KeyZoom( SPEED[ 2 ] );
+		keyZoomOutNormal = new KeyZoom( -SPEED[ 0 ] );
+		keyZoomOutFast = new KeyZoom( -SPEED[ 1 ] );
+		keyZoomOutSlow = new KeyZoom( -SPEED[ 2 ] );
 	}
 
 	@Override
 	public void install( final Behaviours behaviours )
 	{
 		behaviours.behaviour( dragTranslate, DRAG_TRANSLATE, DRAG_TRANSLATE_KEYS );
+		behaviours.behaviour( dragRotate, DRAG_ROTATE,  DRAG_ROTATE_KEYS );
+		
+		behaviours.behaviour( scrollTranslateNormal, SCROLL_TRANSLATE, SCROLL_TRANSLATE_KEYS );
+		behaviours.behaviour( scrollTranslateFast, SCROLL_TRANSLATE_FAST, SCROLL_TRANSLATE_FAST_KEYS );
+		behaviours.behaviour( scrollTranslateSlow, SCROLL_TRANSLATE_SLOW, SCROLL_TRANSLATE_SLOW_KEYS );
+		
 		behaviours.behaviour( zoom, ZOOM_NORMAL, ZOOM_NORMAL_KEYS );
-		behaviours.behaviour( zoom, ZOOM_NORMAL, ZOOM_NORMAL_KEYS );
+		behaviours.behaviour( zoomFast, ZOOM_FAST, ZOOM_FAST_KEYS );
+		behaviours.behaviour( zoomSlow, ZOOM_SLOW, ZOOM_SLOW_KEYS );
 
-		behaviours.namedBehaviour( selectRotationAxisXBehaviour, SELECT_AXIS_X_KEYS );
-		behaviours.namedBehaviour( selectRotationAxisYBehaviour, SELECT_AXIS_Y_KEYS );
-		behaviours.namedBehaviour( selectRotationAxisZBehaviour, SELECT_AXIS_Z_KEYS );
-		behaviours.namedBehaviour( dragRotateBehaviour, DRAG_ROTATE_KEYS );
-		behaviours.namedBehaviour( dragRotateFastBehaviour, DRAG_ROTATE_FAST_KEYS );
-		behaviours.namedBehaviour( dragRotateSlowBehaviour, DRAG_ROTATE_SLOW_KEYS );
-		behaviours.namedBehaviour( translateZBehaviour, SCROLL_Z_KEYS );
-		behaviours.namedBehaviour( translateZFastBehaviour, SCROLL_Z_FAST_KEYS );
-		behaviours.namedBehaviour( translateZSlowBehaviour, SCROLL_Z_SLOW_KEYS );
-		behaviours.namedBehaviour( rotateLeftBehaviour, ROTATE_LEFT_KEYS );
-		behaviours.namedBehaviour( rotateLeftFastBehaviour, ROTATE_LEFT_FAST_KEYS );
-		behaviours.namedBehaviour( rotateLeftSlowBehaviour, ROTATE_LEFT_SLOW_KEYS );
-		behaviours.namedBehaviour( rotateRightBehaviour, ROTATE_RIGHT_KEYS );
-		behaviours.namedBehaviour( rotateRightFastBehaviour, ROTATE_RIGHT_FAST_KEYS );
-		behaviours.namedBehaviour( rotateRightSlowBehaviour, ROTATE_RIGHT_SLOW_KEYS );
-		behaviours.namedBehaviour( keyZoomInBehaviour, KEY_ZOOM_IN_KEYS );
-		behaviours.namedBehaviour( keyZoomInFastBehaviour, KEY_ZOOM_IN_FAST_KEYS );
-		behaviours.namedBehaviour( keyZoomInSlowBehaviour, KEY_ZOOM_IN_SLOW_KEYS );
-		behaviours.namedBehaviour( keyZoomOutBehaviour, KEY_ZOOM_OUT_KEYS );
-		behaviours.namedBehaviour( keyZoomOutFastBehaviour, KEY_ZOOM_OUT_FAST_KEYS );
-		behaviours.namedBehaviour( keyZoomOutSlowBehaviour, KEY_ZOOM_OUT_SLOW_KEYS );
-		behaviours.namedBehaviour( keyForwardZBehaviour, KEY_FORWARD_Z_KEYS );
-		behaviours.namedBehaviour( keyForwardZFastBehaviour, KEY_FORWARD_Z_SLOW_KEYS );
-		behaviours.namedBehaviour( keyForwardZSlowBehaviour, KEY_FORWARD_Z_FAST_KEYS );
-		behaviours.namedBehaviour( keyBackwardZBehaviour, KEY_BACKWARD_Z_KEYS );
-		behaviours.namedBehaviour( keyBackwardZFastBehaviour, KEY_BACKWARD_Z_FAST_KEYS );
-		behaviours.namedBehaviour( keyBackwardZSlowBehaviour, KEY_BACKWARD_Z_SLOW_KEYS );
+		behaviours.behaviour( scrollRotateNormal, SCROLL_ROTATE, SCROLL_ROTATE_KEYS );
+		behaviours.behaviour( scrollRotateFast, SCROLL_ROTATE_FAST, SCROLL_ROTATE_FAST_KEYS );
+		behaviours.behaviour( scrollRotateSlow, SCROLL_ROTATE_SLOW, SCROLL_ROTATE_SLOW_KEYS );
+
+		behaviours.behaviour( keyRotateLeftNormal, ROTATE_LEFT, ROTATE_LEFT_KEYS );
+		behaviours.behaviour( keyRotateLeftFast, ROTATE_LEFT_FAST, ROTATE_LEFT_FAST_KEYS );
+		behaviours.behaviour( keyRotateLeftSlow, ROTATE_LEFT_SLOW, ROTATE_LEFT_SLOW_KEYS );
+		behaviours.behaviour( keyRotateRightNormal, ROTATE_RIGHT, ROTATE_RIGHT_KEYS );
+		behaviours.behaviour( keyRotateRightFast, ROTATE_RIGHT_FAST, ROTATE_RIGHT_FAST_KEYS );
+		behaviours.behaviour( keyRotateRightSlow, ROTATE_RIGHT_SLOW, ROTATE_RIGHT_SLOW_KEYS );
+
+		behaviours.behaviour( keyZoomInNormal,  KEY_ZOOM_IN, KEY_ZOOM_IN_KEYS );
+		behaviours.behaviour( keyZoomInFast, KEY_ZOOM_IN_FAST, KEY_ZOOM_IN_FAST_KEYS );
+		behaviours.behaviour( keyZoomInSlow, KEY_ZOOM_IN_SLOW, KEY_ZOOM_IN_SLOW_KEYS );
+		behaviours.behaviour( keyZoomOutNormal, KEY_ZOOM_OUT, KEY_ZOOM_OUT_KEYS );
+		behaviours.behaviour( keyZoomOutFast, KEY_ZOOM_OUT_FAST, KEY_ZOOM_OUT_FAST_KEYS );
+		behaviours.behaviour( keyZoomOutSlow, KEY_ZOOM_OUT_SLOW, KEY_ZOOM_OUT_SLOW_KEYS );
 	}
 
 
@@ -387,12 +444,20 @@ public class BehaviourTransformEventHandler2DMamut implements BehaviourTransform
 
 	private class ScrollTranslate implements ScrollBehaviour
 	{
+
+		private final double speed;
+
+		public ScrollTranslate( final double speed )
+		{
+			this.speed = speed;
+		}
+
 		@Override
 		public void scroll( final double wheelRotation, final boolean isHorizontal, final int x, final int y )
 		{
 			synchronized ( affine )
 			{
-				final double d = -wheelRotation * 10;
+				final double d = -wheelRotation * 10 * speed;
 				if ( isHorizontal )
 					affine.translate( d, 0, 0 );
 				else
@@ -405,7 +470,12 @@ public class BehaviourTransformEventHandler2DMamut implements BehaviourTransform
 	private class Zoom implements ScrollBehaviour
 	{
 
-		private final double speed = 1.0;
+		private final double speed;
+
+		public Zoom( final double speed )
+		{
+			this.speed = speed;
+		}
 
 		@Override
 		public void scroll( final double wheelRotation, final boolean isHorizontal, final int x, final int y )
@@ -413,7 +483,7 @@ public class BehaviourTransformEventHandler2DMamut implements BehaviourTransform
 			synchronized ( affine )
 			{
 				final double s = speed * wheelRotation;
-				final double dScale = 1.0 + 0.05;
+				final double dScale = 1.0 + 0.05 * Math.abs( s );
 				if ( s > 0 )
 					scale( 1.0 / dScale, x, y );
 				else
